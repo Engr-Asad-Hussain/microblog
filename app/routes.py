38bb2020a021972ee72cb9cc3acc6b80cdae42ee
Asad Hussain <asad.h1998@yahoo.com>
2022-08-05 12:34:38 +0500

    created two routes for microblog

---
+++ app/routes.py
@@ -0,0 +1,30 @@
+from flask import render_template, flash, redirect
+from app import app
+from app.forms import LoginForm
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = { 'username': 'Asad Hussain' }
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', user=user, posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login request for a user {}, remember_me {}'.format(
+            form.username.data, form.remember_me.data
+        ))
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form)

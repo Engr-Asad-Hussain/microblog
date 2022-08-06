c596a641684a77708eb7559633f6e08cebda7c7a
Asad Hussain <asad.h1998@yahoo.com>
2022-08-06 10:27:03 +0500

    added flask sqlacademy and migration

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,17 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
-print(app.config)
+# print(app.config)
 
-from app import routes
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from app import routes, models
+
 
 
 if __name__ == '__main__':
--- config.py
+++ config.py
@@ -1,4 +1,11 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+# basedir = D:\protected_resources\microblog
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'default_secret-Key'
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'default_secret-Key')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+# print(Config.SQLALCHEMY_DATABASE_URI)

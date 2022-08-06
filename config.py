cb610d7ea7f0df147106349e0bdbbc924616e2f9
Asad Hussain <asad.h1998@yahoo.com>
2022-08-06 16:40:51 +0500

    added profile feature, gravatar

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY', 'default_secret-Key')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
+        
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 # print(Config.SQLALCHEMY_DATABASE_URI)

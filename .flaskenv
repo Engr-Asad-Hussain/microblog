2aa5384cef694f5ec91dc430d819dcaf5c38080a
Asad Hussain <asad.h1998@yahoo.com>
2022-08-06 18:31:31 +0500

    added error templates, added email sender, added logs-t-file extension

--- .flaskenv
+++ .flaskenv
@@ -1,1 +1,8 @@
 FLASK_APP=microblog.py
+FLASK_DEBUG=False
+FLASK_ENV=production
+MAIL_SERVER=smtp.googlemail.com
+MAIL_PORT=587
+MAIL_USE_TLS=1
+MAIL_USERNAME=asad.auxin@gmail.com
+MAIL_PASSWORD=25100@GoogleAccount
--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,16 @@
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
+from datetime import datetime
 from app import app, db
-from app.forms import LoginForm, RegistrationForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm
 from app.models import User
 
 @app.route('/')
 @app.route('/index')
 @login_required
 def index():
+    print('/index route is executing ...')
     user = { 'username': 'Asad Hussain' }
     posts = [
         {
@@ -25,6 +27,7 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    print('/login route is executing ...')
     if current_user.is_authenticated:
         return redirect(url_for('index'))
     form = LoginForm()
@@ -45,12 +48,14 @@ def login():
 
 @app.route('/logout')
 def logout():
+    print('/logout route is executing ...')
     logout_user()
     return redirect(url_for('index'))
 
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    print('/register route is executing ...')
     if current_user.is_authenticated:
         return redirect(url_for('index'))
     form = RegistrationForm()
@@ -67,9 +72,38 @@ def register():
 @app.route('/user/<username>')
 @login_required
 def user(username):
+    print('/user/<username> route is executing ...')
     user = User.query.filter_by(username=username).first_or_404()
     posts = [
         {'author': user, 'body': 'Test post #1'},
         {'author': user, 'body': 'Test post #2'}
     ]
     return render_template('user.html', user=user, posts=posts)
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile',
+                           form=form)
+
+@app.before_request
+def before_request():
+    """ The @before_request decorator from Flask register the decorated function to be 
+    executed right before the view function. This is extremely useful because now I can 
+    insert code that I want to execute before any view function in the application, and 
+    I can have it in a single place."""
+    
+    print('before_request is executed')
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
